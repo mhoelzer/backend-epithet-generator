6ae488656b4fb7ed4b830d20b5a28b8d3c9745be
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-18 15:02:33 -0400

    connected to helpers

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ __author__ = "mhoelzer"
 
 from flask import Flask
 from flask import jsonify
-from dotenv import load_dotenv
-
-import os
+# from dotenv import load_dotenv
+from .helpers import EpithetGenerator, Vocabulary
+# . does b/c it goes out to parent then back; changed where py looks; not where running but same dir where this is; relative import
 
 app = Flask(__name__)
 
 # tells flask what name of file is but flask needs to find it; will need .env later but not right now
-load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+# load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 # ^^^ links with env file; ^^^if fork, can run based off computer
 # FLASK_APP = os.environ.get("FLASK_APP")
 # FLASK_ENV = os.environ.get("FLASK_ENV")
@@ -25,11 +25,14 @@ load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 @app.route("/")
 def generate_epithets():
-    epithets = {"epithets": []}
-    return jsonify(epithets)
+    result = EpithetGenerator().generate()
+    # epithets = {"epithets": result}
+    return jsonify({"epithets": result})
 
 
 @app.route("/vocabulary")
 def vocabulary():
-    vocabulary = {"vocabulary": {}}
-    return jsonify(vocabulary)
+    # vocabulary = {"vocabulary": {}}
+    # return jsonify(vocabulary)
+    result = Vocabulary().read_json()
+    return jsonify({"vocabulary": result})
