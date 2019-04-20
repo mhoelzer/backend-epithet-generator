46d7603e7e8ea435b6fc39e38a6c2099687dfed0
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-20 17:09:46 -0400

    random num of epithets functioning

--- app.py
+++ app.py
@@ -10,36 +10,35 @@ __author__ = "mhoelzer"
 
 from flask import Flask
 from flask import jsonify
-# from dotenv import load_dotenv
 from backend_epithet_generator.helpers import EpithetGenerator, Vocabulary
-# . does b/c it goes out to parent then back; changed where py looks; not where running but same dir where this is; relative import
+# ^^^ . does b/c it goes out to parent then back; changed where py looks; 
+# not where running but same dir where this is; relative import
+
+import random
 
 app = Flask(__name__)
-
-# tells flask what name of file is but flask needs to find it; will need .env later but not right now
-# load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
-# ^^^ links with env file; ^^^if fork, can run based off computer
-# FLASK_APP = os.environ.get("FLASK_APP")
-# FLASK_ENV = os.environ.get("FLASK_ENV")
 
 
 @app.route("/")
 def generate_epithets():
     result = EpithetGenerator().generate_one()
-    # epithets = {"epithets": result}
     return jsonify({"epithets": result})
 
 
 @app.route("/vocabulary")
 def vocabulary():
-    # vocabulary = {"vocabulary": {}}
-    # return jsonify(vocabulary)
     result = Vocabulary.read_json("resources/data.json")
     return jsonify({"vocabulary": result})
 
 
 @app.route("/epithets/<int:quantity>")
-def generate_multiple_epithets(quantity=1):
+def generate_multiple_epithets(quantity):
     result = EpithetGenerator().generate_multi(quantity)
-    # epithets = {"epithets": result}
+    return jsonify({"epithets": result})
+
+
+@app.route("/randomizeme")
+def generate_random_num_epithets():
+    random_times = random.randint(1, 333)
+    result = EpithetGenerator().generate_multi(random_times)
     return jsonify({"epithets": result})
