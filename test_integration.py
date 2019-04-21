cc1b03208a93f76c2de6f06ffda22b3f8b339d29
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-21 00:44:23 -0400

    integration tests should be completed....

--- test_integration.py
+++ test_integration.py
@@ -1,19 +1,26 @@
 from backend_epithet_generator.app import app
-from backend_epithet_generator.helpers import Vocabulary as V
-import json
-
-json_data = V.read_json("resources/data.json")
 
 
 def test_app_gen_one():
     with app.test_client() as cli:
-        response = cli.get("/")
-        data = response.data.decode()
+        result = cli.get("/")
+        assert result.status_code == 200
+        data = result.data.decode()
         assert isinstance(data, str)
-        result = json.loads(data)
-        assert response.status_code == 200
-        assert "epithets" in result
 
-# def test_app_vocab():
-#     result = app.test_client().get("/vocabulary")
-#     assert result.data.decode() == json_data
+
+def test_app_vocab():
+    result = app.test_client().get("/vocabulary")
+    assert result.status_code == 200
+    assert result.data.decode()
+
+
+def test_app_gen_multi():
+    result = app.test_client().get("/epithets/<int:quantity>")
+    assert result.data.decode()
+
+
+def test_app_randomizeme():
+    result = app.test_client().get("/randomizeme")
+    assert result.status_code == 200
+    assert result.data.decode()
